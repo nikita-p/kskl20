b4b113b2c2ab625157e25714e77aa18e643c6180
nikita-p <nikita.petrov.1997@gmail.com>
2020-10-12 22:38:23 +0700

    Add comparison check #4

--- notebooks/pylib/style.py
+++ notebooks/pylib/style.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def my_style(title='', xtitle='', ytitle='', gridstyle=':', legend=False):
+def my_style(title='', xtitle='', ytitle='', gridstyle='--', legend=False):
     """Стиль для адекватного простого отображения картинок"""
-    plt.grid(linestyle=gridstyle)
     plt.title(title)
     plt.xlabel(xtitle)
     plt.ylabel(ytitle)
+    plt.grid(which='major', linestyle=gridstyle)
+    plt.minorticks_on()
+    plt.grid(which='minor', linestyle=':', alpha=0.5)
     if legend:
         plt.legend(frameon=True)
+    plt.tight_layout()
     
 def hep_histo(data, bins=10, range=None, label=None):
     """Гистограмма, наиболее похожая на то, что нужно в ФЭЧ"""
