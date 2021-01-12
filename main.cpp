3c7f2195c56c660d8ed18abfa6361b66915025ae
nikita-p <nikita.petrov.1997@gmail.com>
2021-01-12 23:51:43 +0700

    Return to rho cut

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char *argv[]){
   Washer a(argv[1]);
   a.Loop({&Washer::FilterNTracks, &Washer::FilterNKaons, &Washer::FilterBadRun, 
-          &Washer::FilterDeDx, &Washer::FilterHits, //&Washer::FilterRho,
+          &Washer::FilterDeDx, &Washer::FilterHits, &Washer::FilterRho,
           &Washer::FilterBestMass, &Washer::FilterKaonTracks});
   a.Save(argv[2]);
   return 0;
